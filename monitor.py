1a49845b8f80799095632af23eeb7330729f6c4c
xton13 <mike.cutshaw@hotmail.com>
2017-12-03 07:03:40 +0000

    finished

--- monitor.py
+++ monitor.py
@@ -5,12 +5,13 @@ import subprocess
 max_fails = 20
 
 if(len(sys.argv)) < 2:
-    print("Usage: "+str(sys.argv[0])+" \"COMMAND\" LOGFILE")
+    print("Usage: "+str(sys.argv[0])+" \"COMMAND\"")
     exit()
 args = str(sys.argv[1])
 while(1):
     try:
-        subprocess.call(args,shell=True)
+        proc = subprocess.Popen(args,shell=True)
+        proc.wait()
         max_fails-=1
         if(max_fails == 0):
             print("Max Failures exceeded, exiting")
